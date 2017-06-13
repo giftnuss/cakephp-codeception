7290def1762eec8d652ddd4bf2295ad39c9037bd
Sebastian B. Knapp <news@young-workers.de>
2017-06-13 15:17:16 +0200

    use an independent before_install script to facilitate local tests

---
+++ scripts/before_install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+if [ -f .git/shallow ]; then rm .git/shallow; fi
+if [ -z "$CAKE_VERSION" ]; then CAKE_VERSION=3.4.* ; fi
+if [ -d tests/test_app ]; then rm -rf tests/test_app ; fi
+if [ -z "$TRAVIS_BRANCH" ]; then TRAVIS_BRANCH=$(git rev-parse --abbrev-ref HEAD) ; fi
+if [ -z "$TRAVIS_COMMIT" ]; then TRAVIS_COMMIT=$(git log --format="%H" -n 1); fi
+
+composer install
+composer require --dev squizlabs/php_codesniffer:~2.0
+composer create-project --prefer-source -s dev --no-interaction cakephp/app:$CAKE_VERSION tests/test_app
+cd tests/test_app
+composer config repositories.local vcs ../../
+composer require --prefer-source --dev cakephp/codeception:dev-$TRAVIS_BRANCH#$TRAVIS_COMMIT
+rm composer.lock
+composer install --prefer-source
+vendor/bin/codecept bootstrap
+vendor/bin/codecept generate:cest functional Foo
+vendor/bin/codecept generate:cept functional Foo
+cd ../sample_app
+cp ../test_app/composer.json .
+cp ../test_app/src/Console/Installer.php src/Console/
+composer install --prefer-source
